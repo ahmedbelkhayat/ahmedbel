f14f39a573aa31758d7499a3f29305467f9b6022
unknown <hanaa1504@hotmail.com>
2019-08-31 19:34:05 +0200

    all changes

--- src/app/displaypic/displaypic.component.ts
+++ src/app/displaypic/displaypic.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PicturesService } from '../pictures.service';
+import { Picture } from '../picture';
 
 @Component({
   selector: 'lw-displaypic',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./displaypic.component.css']
 })
 export class DisplaypicComponent implements OnInit {
-
-  constructor() { }
+   pictures : Picture[];
+  constructor(private prictureService:PicturesService) { }
 
   ngOnInit() {
+    this.getAllPictures();
   }
+  getAllPictures() : void {
+    this.prictureService.getPictrures().subscribe(pictures => this.pictures = pictures);
+
+  }
+  getPicturesByCategory(category:string) : void {
+    this.prictureService.getPicturesByCateroy(category).subscribe(pictures => this.pictures = pictures);
+
+  }
+  
 
 }
