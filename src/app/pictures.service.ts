e7dc1de698a289b47b4294bbf40efd6ef76be799
unknown <hanaa1504@hotmail.com>
2019-08-31 17:02:37 +0200

    service picture

--- src/app/pictures.service.ts
+++ src/app/pictures.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@angular/core';
+import { Picture } from './picture';
+import {PICTURES} from './mockPictures';
+import { Observable, of } from 'rxjs';
+import { catchError} from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +11,36 @@ import { Injectable } from '@angular/core';
 export class PicturesService {
 
   constructor() { }
+
+  getPictrures(): Observable<Picture[]> {
+    return of(PICTURES)
+    .pipe(catchError(this.handleError('getPicturesByCategory()', [])));
+  }
+  getPicturesByCateroy(category:string){
+    return of(PICTURES.filter(picture => picture.categorie.includes(category)))
+    .pipe(catchError(this.handleError('getPicturesByCategory()', []))
+    );
+  }
+
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+private handleError<T> (operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+
+    // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+
+    // TODO: better job of transforming error for user consumption
+
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+  };
+}
+
+
+
 }
