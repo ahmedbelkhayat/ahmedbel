dd41471466454687d28798e15b9b574aea5795e9
unknown <hanaa1504@hotmail.com>
2019-08-31 23:02:01 +0200

    major changes

--- src/app/displaypic/displaypic.component.ts
+++ src/app/displaypic/displaypic.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { PicturesService } from '../pictures.service';
 import { Picture } from '../picture';
+import { NgbDropdown} from '@ng-bootstrap/ng-bootstrap';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'lw-displaypic',
@@ -9,10 +12,20 @@ import { Picture } from '../picture';
 })
 export class DisplaypicComponent implements OnInit {
    pictures : Picture[];
-  constructor(private prictureService:PicturesService) { }
+   categoryFormFirst:string;
+  constructor(private prictureService:PicturesService,private route: ActivatedRoute) {
+    this.route.queryParams.subscribe(params => {
+    this.categoryFormFirst = params['category']
+   });}
 
   ngOnInit() {
+    if(this.categoryFormFirst ==null){
     this.getAllPictures();
+  }
+    else
+    {
+     this.getPicturesByCategory(this.categoryFormFirst);
+    }
   }
   getAllPictures() : void {
     this.prictureService.getPictrures().subscribe(pictures => this.pictures = pictures);
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'lw-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.css']
+  styleUrls: ['./portfolio.component.css'],
+
 })
 export class PortfolioComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
+  getCategory(category:string) : void{
+    let navigationExtras: NavigationExtras = {
+        queryParams: {
+            "category": category,
+        }
+    };
+    this.router.navigate(["/portfolio"], navigationExtras);
+}
 
 }
