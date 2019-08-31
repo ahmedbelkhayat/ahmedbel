7e2a215b7373c3873cd0d4caae46474d797bbefa
unknown <hanaa1504@hotmail.com>
2019-08-31 16:22:50 +0200

    commit

---
+++ src/app/mockPictures.ts
@@ -0,0 +1,44 @@
+import { Picture } from './picture';
+
+export const PICTURES: Picture[] = [
+    {id:1,name:'Complexe',url: '../../assests/images/macro/macro2.JPG',categorie:['macro']},
+    {id:2,name:'Precision',url: '../../assests/images/macro/macro3.JPG',categorie:['macro']},
+    {id:3,name:'Flying insect',url: '../../assests/images/macro/macro4.JPG',categorie:['macro']},
+    {id:4,name:'The welcoming sun',url: '../../assests/images/macro/macro5.JPG',categorie:['macro']},
+    {id:5,name:'The welcoming sun',url: '../../assests/images/macro/macro.JPG',categorie:['macro','bn']},
+    {id:6,name:'Focus',url: '../../assests/images/bn/bw1.JPG',categorie:['bn']},
+    {id:29,name:'Jazz Concert',url: '../../assests/images/jazz/concert3.JPG',categorie:['concert','bn']},
+    {id:8,name:'Peur',url: '../../assests/images/bn/bw3.JPG',categorie:['bn']},
+    {id:28,name:'Jazz Concert',url: '../../assests/images/jazz/concert2.JPG',categorie:['concert']},
+    {id:9,name:'Bravour',url: '../../assests/images/bn/bw4.JPG',categorie:['bn']},
+    {id:32,name:'Jazz Concert',url: '../../assests/images/jazz/concert6.JPG',categorie:['concert','bn']},
+    {id:10,name:'Malheur',url: '../../assests/images/bn/bw5.JPG',categorie:['bn']},
+    {id:11,name:'Joie',url: '../../assests/images/mariage/mariage.JPG',categorie:['mariage','bn']},
+    {id:12,name:'Sourire',url: '../../assests/images/mariage/mariage13.JPG',categorie:['mariage','bn']},
+    {id:7,name:'Action',url: '../../assests/images/bn/bw2.JPG',categorie:['bn']},
+    {id:17,name:'Tendresse',url: '../../assests/images/mariage/mariage11.JPG',categorie:['mariage','bn']},
+    {id:24,name:'Joueurs',url: '../../assests/images/portrait/portrait13.JPG',categorie:['portrait']},
+    {id:13,name:'Protection',url: '../../assests/images/mariage/mariage9.JPG',categorie:['mariage','bn']},
+    {id:14,name:'Pour la vie',url: '../../assests/images/mariage/mariage7.JPG',categorie:['mariage']},
+    {id:15,name:'Future',url: '../../assests/images/mariage/mariage4.JPG',categorie:['mariage','bn']},
+    {id:33,name:'Jazz Concert',url: '../../assests/images/jazz/concert7.JPG',categorie:['concert','bn']},
+    {id:16,name:'Galenterie',url: '../../assests/images/mariage/mariage10.JPG',categorie:['mariage','bn']},
+    {id:25,name:'Percant',url: '../../assests/images/portrait/portrait9.JPG',categorie:['portrait']},
+    {id:18,name:'Amour',url: '../../assests/images/mariage/mariage12.JPG',categorie:['mariage','bn']},
+    {id:35,name:'Jazz Concert',url: '../../assests/images/jazz/concert9.JPG',categorie:['concert','bn']},
+    {id:19,name:'Serinite',url: '../../assests/images/portrait/baby2.JPG',categorie:['portrait','bn']},
+    {id:20,name:'Curiosite',url: '../../assests/images/portrait/protrait3.JPG',categorie:['portrait','bn']},
+    {id:21,name:'Concentration',url: '../../assests/images/portrait/portrait1.JPG',categorie:['portrait']},
+    {id:31,name:'Jazz Concert',url: '../../assests/images/jazz/concert5.JPG',categorie:['concert']},
+    {id:22,name:'Sy Fi',url: '../../assests/images/portrait/portrait7.JPG',categorie:['portrait']},
+    {id:23,name:'En famille',url: '../../assests/images/portrait/portrait12.JPG',categorie:['portrait']},
+    {id:30,name:'Jazz Concert',url: '../../assests/images/jazz/concert4.JPG',categorie:['concert','bn']},
+    {id:26,name:'Douceur',url: '../../assests/images/portrait/baby1.JPG',categorie:['portrait']},
+    {id:27,name:'Jazz Concert',url: '../../assests/images/jazz/concert1.JPG',categorie:['concert']},
+    {id:34,name:'Jazz Concert',url: '../../assests/images/jazz/concert8.JPG',categorie:['concert','bn']},
+    
+    
+]
+    
+
+
